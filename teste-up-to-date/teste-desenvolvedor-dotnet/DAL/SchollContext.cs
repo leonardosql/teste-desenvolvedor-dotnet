cb13680e4e7d3b19d6759c5f602f2cd0f34423b1
Leonardo Ackerman <am.leojunior@gmail.com>
2023-03-21 18:00:17 -0300

    adiçãode context do modelo de dados

---
+++ teste-up-to-date/teste-desenvolvedor-dotnet/DAL/SchollContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using teste_desenvolvedor_dotnet.Models;
+
+namespace teste_desenvolvedor_dotnet.DAL
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
